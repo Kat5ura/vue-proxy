f3c1cd53fe8d6505b244439cb416e9f846dc1559
Kat5ura <971969499@qq.com>
2019-01-08 11:07:31 +0800

    feat(background):添加请求监听逻辑

--- src/background.js
+++ src/background.js
@@ -10,3 +10,28 @@ ext.runtime.onMessage.addListener(
     }
   }
 );
+
+const filter = {
+  urls: ['<all_urls>']
+}
+
+const events = {
+  onBeforeRequest: ['blocking', 'requestBody'],
+  onBeforeSendHeaders: ['requestHeaders', 'blocking'],
+  onSendHeaders: ['requestHeaders'],
+  onHeadersReceived: ['responseHeaders', 'blocking'],
+  onResponseStarted: ['responseHeaders'],
+  onBeforeRedirect: ['responseHeaders'],
+  onCompleted: ['responseHeaders']
+}
+
+Object.keys(events).forEach((event) => {
+  let options = events[event]
+  ext.webRequest[event].addListener(function(details){
+    console.log(event + ': ',  details)
+  }, filter, options)
+})
+
+ext.webRequest.onErrorOccurred.addListener(function (detailes) {
+  console.log('error: ', detailes)
+}, filter)
